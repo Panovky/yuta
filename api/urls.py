227ec8c0b88f935373330755f6260f7fcb9e0a75
Ksenia Panova <rybinak95@gmail.com>
2024-03-29 21:39:42 +0300

    Add slashes to urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('authorization', views.AuthorizationView.as_view()),
-    path('search', views.SearchView.as_view()),
-    path('profile', views.ProfileView.as_view()),
-    path('projects', views.ProjectsView.as_view()),
-    path('tasks', views.TasksView.as_view()),
-    path('teams', views.TeamsView.as_view())
+    path('authorization/', views.AuthorizationView.as_view()),
+    path('search/', views.SearchView.as_view()),
+    path('profile/', views.ProfileView.as_view()),
+    path('projects/', views.ProjectsView.as_view()),
+    path('tasks/', views.TasksView.as_view()),
+    path('teams/', views.TeamsView.as_view())
 ]
