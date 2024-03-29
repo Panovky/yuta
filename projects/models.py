019e0ee516b9c9343a5619b51bb8ad42846a703c
Ksenia Panova <rybinak95@gmail.com>
2024-03-29 22:25:13 +0300

    Create file path getter

--- projects/models.py
+++ projects/models.py
@@ -1,12 +1,8 @@
 import datetime
-import re
-from django.core.files.storage import FileSystemStorage
 from django.db import models
-from YUTA.settings import MEDIA_ROOT
 from users.models import User
 from teams.models import Team
-from transliterate import slugify
-
+from services.file_path_getter import get_file_path
 
 STATUS_CHOICES = (
     ('в работе', 'в работе'),
@@ -15,54 +11,20 @@ STATUS_CHOICES = (
 )
 
 
+def get_photo_path(instance, file_name):
+    return get_file_path(file_name, 'images/projects_photos')
+
+
+def get_technical_task_path(instance, file_name):
+    return get_file_path(file_name, 'projects_technical_tasks')
+
+
 class Project(models.Model):
-    def get_photo_path(self, file_name):
-        fs = FileSystemStorage()
-        file_names = fs.listdir(path=f'{MEDIA_ROOT}\\images\\projects_photos')[1]
-
-        extension = file_name[file_name.rfind('.') + 1:]
-        slug = slugify(file_name.replace(f'.{extension}', ''), language_code='ru')
-        file_name = f'{slug}.{extension}'
-
-        if file_name not in file_names:
-            return f'images/projects_photos/{file_name}'
-        else:
-            pattern = f'{slug}-\\d+\\.{extension}'
-            same_file_names = [file_name for file_name in file_names if re.fullmatch(pattern, file_name)]
-            if not same_file_names:
-                return f'images/projects_photos/{slug}-1.{extension}'
-            versions = [
-                int(file_name.replace(f'{slug}-', '').replace(f'.{extension}', ''))
-                for file_name in same_file_names
-            ]
-            return f'images/projects_photos/{slug}-{max(versions) + 1}.{extension}'
-
-    def get_technical_task_path(self, file_name):
-        fs = FileSystemStorage()
-        file_names = fs.listdir(path=f'{MEDIA_ROOT}\\projects_technical_tasks\\')[1]
-
-        extension = file_name[file_name.rfind('.') + 1:]
-        slug = slugify(file_name.replace(f'.{extension}', ''), language_code='ru')
-        file_name = f'{slug}.{extension}'
-
-        if file_name not in file_names:
-            return f'projects_technical_tasks/{file_name}'
-        else:
-            pattern = f'{slug}-\\d+\\.{extension}'
-            same_file_names = [file_name for file_name in file_names if re.fullmatch(pattern, file_name)]
-            if not same_file_names:
-                return f'projects_technical_tasks/{slug}-1.{extension}'
-            versions = [
-                int(file_name.replace(f'{slug}-', '').replace(f'.{extension}', ''))
-                for file_name in same_file_names
-            ]
-            return f'projects_technical_tasks/{slug}-{max(versions) + 1}.{extension}'
-
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
-    photo = models.ImageField(blank=True, upload_to=get_photo_path, default='images/project_default.png')
+    photo = models.ImageField(blank=True, upload_to=get_photo_path, default='images/project-default-photo.png')
     description = models.CharField(max_length=150)
-    technical_task = models.FileField(blank=True, null=True, upload_to=get_technical_task_path,)
+    technical_task = models.FileField(blank=True, null=True, upload_to=get_technical_task_path)
     creation_date = models.DateField(auto_now_add=True)
     deadline = models.DateField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='в работе')
