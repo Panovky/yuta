9c1f079fbedebc51e2d55b722d5ab14732c0a67d
Ksenia Panova <rybinak95@gmail.com>
2024-04-27 15:01:02 +0300

    Production settings

--- YUTA/settings.py
+++ YUTA/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_kbo!w08pz^g^9sue!e&7+9oia@8&_x(sl6j@@63pmnn6!xn*h'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_kbo!w08pz^g^9sue!e&7+9oia@8&_x(sl6j@@63pmnn6!xn*h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
